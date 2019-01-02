416c362bbd5af4c44d6f82a893b67dd5e598a5d7
vhots <vasyl.hotsuliak@gmail.com>
2019-01-02 10:41:43 +0200

    add TemplateHealthCheck

--- src/main/java/com/vhots/dropwizard/helloworld/HelloWorldApplication.java
+++ src/main/java/com/vhots/dropwizard/helloworld/HelloWorldApplication.java
@@ -1,11 +1,13 @@
 package com.vhots.dropwizard.helloworld;
 
-import com.vhots.dropwizard.helloworld.config.DropwizardJavaSimpleConfiguration;
+import com.vhots.dropwizard.helloworld.config.HelloWorldConfiguration;
+import com.vhots.dropwizard.helloworld.health.TemplateHealthCheck;
+import com.vhots.dropwizard.helloworld.resources.HelloWorldResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
-public class HelloWorldApplication extends Application<DropwizardJavaSimpleConfiguration> {
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
 
     public static void main(final String[] args) throws Exception {
         new HelloWorldApplication().run(args);
@@ -17,14 +19,20 @@ public class HelloWorldApplication extends Application<DropwizardJavaSimpleConfi
     }
 
     @Override
-    public void initialize(final Bootstrap<DropwizardJavaSimpleConfiguration> bootstrap) {
+    public void initialize(final Bootstrap<HelloWorldConfiguration> bootstrap) {
         // TODO: application initialization
     }
 
     @Override
-    public void run(final DropwizardJavaSimpleConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
+    public void run(HelloWorldConfiguration configuration,
+                    Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        final TemplateHealthCheck healthCheck =
+                new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
     }
-
 }
--- src/main/java/com/vhots/dropwizard/helloworld/config/HelloWorldConfiguration.java
+++ src/main/java/com/vhots/dropwizard/helloworld/config/HelloWorldConfiguration.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 import org.hibernate.validator.constraints.NotEmpty;
 
-public class DropwizardJavaSimpleConfiguration extends Configuration {
+public class HelloWorldConfiguration extends Configuration {
     @NotEmpty
     private String template;
 
